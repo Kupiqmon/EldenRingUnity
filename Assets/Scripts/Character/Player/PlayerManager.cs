402df40f2236760f97f503b2c051b6e2ec51dcf0
qnguyen93 <quihuynhquang.nguyen@dxc.com>
2024-04-27 23:30:52 +0700

    baseline EP 15 - Elden Ring Unity

---
+++ Assets/Scripts/Character/Player/PlayerManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SG
+{
+    public class PlayerManager : CharacterManager
+    {
+        [HideInInspector] public PlayerAnimatorManager playerAnimatorManager;
+        [HideInInspector] public PlayerLocomotionManager playerLocomotionManager;
+        [HideInInspector] public PlayerNetworkManager playerNetworkManager;
+        [HideInInspector] public PlayerStatsManager playerStatsManager;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            //  DO MORE STUFF, ONLY FOR THE PLAYER
+
+            playerLocomotionManager = GetComponent<PlayerLocomotionManager>();
+            playerAnimatorManager = GetComponent<PlayerAnimatorManager>();
+            playerNetworkManager = GetComponent<PlayerNetworkManager>();
+            playerStatsManager = GetComponent<PlayerStatsManager>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            //  IF WE DO NOT OWN THIS GAMEOBJECT, WE DO NOT CONTROL OR EDIT IT
+            if (!IsOwner)
+                return;
+
+            //  HANDLE MOVEMENT
+            playerLocomotionManager.HandleAllMovement();
+
+            //  REGEN STAMINA
+            playerStatsManager.RegenerateStamina();
+        }
+
+        protected override void LateUpdate()
+        {
+            if (!IsOwner)
+                return;
+
+            base.LateUpdate();
+
+            PlayerCamera.instance.HandleAllCameraActions();
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+
+            //  IF THIS IS THE PLAYER OBJECT OWNED BY THIS CLIENT
+            if (IsOwner)
+            {
+                PlayerCamera.instance.player = this;
+                PlayerInputManager.instance.player = this;
+                WorldSaveGameManager.instance.player = this;
+
+                playerNetworkManager.currentStamina.OnValueChanged += PlayerUIManager.instance.playerUIHudManager.SetNewStaminaValue;
+                playerNetworkManager.currentStamina.OnValueChanged += playerStatsManager.ResetStaminaRegenTimer;
+
+                //  THIS WILL BE MOVED WHEN SAVING AND LOADING IS ADDED
+                playerNetworkManager.maxStamina.Value = playerStatsManager.CalculateStaminaBasedOnEnduranceLevel(playerNetworkManager.endurance.Value);
+                playerNetworkManager.currentStamina.Value = playerStatsManager.CalculateStaminaBasedOnEnduranceLevel(playerNetworkManager.endurance.Value);
+                PlayerUIManager.instance.playerUIHudManager.SetMaxStaminaValue(playerNetworkManager.maxStamina.Value);
+            }
+        }
+
+        public void SaveGameDataToCurrentCharacterData(ref CharacterSaveData currentCharacterData)
+        {
+            currentCharacterData.sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            currentCharacterData.characterName = playerNetworkManager.characterName.Value.ToString();
+            currentCharacterData.xPosition = transform.position.x;
+            currentCharacterData.yPosition = transform.position.y;
+            currentCharacterData.zPosition = transform.position.z;
+        }
+
+        public void LoadGameDataFromCurrentCharacterData(ref CharacterSaveData currentCharacterData)
+        {
+            playerNetworkManager.characterName.Value = currentCharacterData.characterName;
+            Vector3 myPosition = new Vector3(currentCharacterData.xPosition, currentCharacterData.yPosition, currentCharacterData.zPosition);
+            transform.position = myPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/README.txt
@@ -0,0 +1,6 @@
+30:00 - EP 3
+06:30 - EP 4
+12:20 - EP 4
+Please note if you have "rigid" camera movement, change your input to "Analog" instead of "Digital". This is corrected in a future episode.
+28:30 - EP 4
+00:40 - EP 5
